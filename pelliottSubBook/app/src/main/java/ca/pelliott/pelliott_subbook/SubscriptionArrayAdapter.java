5861e4239696c4bebdea04031d795995adef6bbc
Peter Elliott <p.elliott332@gmail.com>
2018-01-25 15:43:28 -0700

    added total to lsit

--- pelliottSubBook/app/src/main/java/ca/pelliott/pelliott_subbook/SubscriptionArrayAdapter.java
+++ pelliottSubBook/app/src/main/java/ca/pelliott/pelliott_subbook/SubscriptionArrayAdapter.java
@@ -17,27 +17,55 @@ import java.util.ArrayList;
 // https://github.com/codepath/android-custom-array-adapter-demo
 
 public class SubscriptionArrayAdapter extends ArrayAdapter<Subscription> {
+
+    private ArrayList<Subscription> subs;
+
     public SubscriptionArrayAdapter(Context context, ArrayList<Subscription> subs) {
         super(context, 0, subs);
+        this.subs = subs;
     }
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-        if (view == null) {
-            // this line was modified from the afformentioned demo
-            view = LayoutInflater.from(getContext()).inflate(R.layout.sub_list_item, parent, false);
+        if (position == 0) {
+            if (view == null) {
+                // this line was modified from the afformentioned demo
+                view = LayoutInflater.from(getContext()).inflate(R.layout.sub_list_total, parent, false);
+            }
+
+            double totalPrice = 0;
+
+            for (Subscription sub: this.subs) {
+                totalPrice += sub.getCharge();
+            }
+
+            TextView priceview = (TextView) view.findViewById(R.id.totalPrice);
+            priceview.setText(String.format("$%.2f", totalPrice));
+
+            return view;
+        } else {
+            position -= 1;
+            if (view == null) {
+                // this line was modified from the afformentioned demo
+                view = LayoutInflater.from(getContext()).inflate(R.layout.sub_list_item, parent, false);
+            }
+
+            Subscription sub = getItem(position);
+
+            TextView name    = (TextView) view.findViewById(R.id.LIname);
+            TextView price   = (TextView) view.findViewById(R.id.LIprice);
+            TextView comment = (TextView) view.findViewById(R.id.LIcomment);
+
+            name.setText(sub.getName());
+            price.setText(String.format("$%.2f", sub.getCharge()));
+            comment.setText(sub.getComment());
+
+            return view;
         }
+    }
 
-        Subscription sub = getItem(position);
-
-        TextView name    = (TextView) view.findViewById(R.id.LIname);
-        TextView price   = (TextView) view.findViewById(R.id.LIprice);
-        TextView comment = (TextView) view.findViewById(R.id.LIcomment);
-
-        name.setText(sub.getName());
-        price.setText(String.format("$%.2f", sub.getCharge()));
-        comment.setText(sub.getComment());
-
-        return view;
+    @Override
+    public int getCount() {
+        return super.getCount() + 1;
     }
 }
